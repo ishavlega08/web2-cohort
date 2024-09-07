a40cf9ce91954bfd0823caaacb7119a8935c3bbc
Ishav <ishavlega@gmail.com>
2024-09-07 12:48:11 +0530

    promises started

--- Week-2/class-2/index.js
+++ Week-2/class-2/index.js
@@ -6,4 +6,16 @@ function callback() {
   console.log("3 seconds have passed");
 }
 
-setTimeoutPromisified(3000).then(callback);
+// setTimeoutPromisified(3000).then(callback);
+function random(resolve) {
+  setTimeout(resolve, 3000);
+}
+
+let p = new Promise(random);
+// console.log(p);
+
+function callback() {
+  console.log("promise succeded");
+}
+
+p.then(callback);
