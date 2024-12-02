0394e43cf2d5a0188beae4b64a0a2807273696bc
Ishav <ishavlega@gmail.com>
2024-12-02 16:21:18 +0530

    hashing and zod

--- Week-7/class-1/index.js
+++ Week-7/class-1/index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const { UserModel, TodoModel } = require("./db");
 const jwt = require("jsonwebtoken");
 const { auth, JWT_SECRET } = require("./auth");
+const bcrypt = require("bcrypt");
+const { z, string } = require("zod");
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,19 +13,44 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", async function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-    const name = req.body.password;
+    try {
+        const requiredBody = z.object({
+            email: z.string().email(),
+            password: z.string().min(3).max(20),
+            name: z.string().min(3).max(20)
+        });
 
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    });
+        const parsedData = requiredBody.safeParse(req.body);
 
-    res.json({
-        message: "You are signed up"
-    })
+        if(!parsedData.success) {
+            res.status(403).json({
+                message: "Invalid format",
+                error: parsedData.error
+            })
+            return
+        }
+
+        const email = req.body.email;
+        const password = req.body.password;
+        const name = req.body.password;
+    
+        const hashedPassword = await bcrypt.hash(password, 5);
+        // console.log(hashedPassword);
+    
+        await UserModel.create({
+            email: email,
+            password: hashedPassword,
+            name: name
+        });
+    
+        res.json({
+            message: "You are signed up"
+        })     
+    } catch (error) {
+        res.status(500).json({
+            mesage: "Error while signing up"
+        })
+    }
 });
 
 app.post("/login", async function(req, res) {
@@ -31,13 +58,20 @@ app.post("/login", async function(req, res) {
     const password = req.body.password;
 
     const user =  await UserModel.findOne({
-        email: email,
-        password: password
+        email: email
     });
 
-    console.log(user);
+    if(!user) {
+        res.status(403).json({
+            message: "User does not exist"
+        });
+        return
+    }
 
-    if(user) {
+    // console.log(user);
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if(passwordMatch) {
         const token = jwt.sign({
             id: user._id.toString()
         }, JWT_SECRET);
@@ -53,20 +87,26 @@ app.post("/login", async function(req, res) {
 });
 
 app.post("/todo", auth, async function(req, res) {
-    const userId = req.userId;
-    const title = req.body.title;
-    const done = req.body.done;
-
-    const todo = await TodoModel.create({
-        title: title,
-        done: done,
-        userId: userId
-    });
-
-    res.json({
-        message: "Todo added successfully",
-        todo: todo
-    })
+    try {
+        const userId = req.userId;
+        const title = req.body.title;
+        const done = req.body.done;
+    
+        const todo = await TodoModel.create({
+            title: title,
+            done: done,
+            userId: userId
+        });
+    
+        res.json({
+            message: "Todo added successfully",
+            todo: todo
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Error while creating todo"
+        })
+    }
 });
 
 app.get("/todos", auth, async function(req, res) {
